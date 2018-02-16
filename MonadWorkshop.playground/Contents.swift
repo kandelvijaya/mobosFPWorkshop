5d44dd37cd2385ccf65b11116335b14e1137c336
vkandel <vijaya.prakash.kandel@zalando.de>
2018-02-16 09:37:06 +0100

    Added functor and docs

--- MonadWorkshop.playground/Contents.swift
+++ MonadWorkshop.playground/Contents.swift
@@ -2,5 +2,49 @@
 
 import UIKit
 
-let result1 = Result<Int>.success(1)
+let res1 = Result.success(1)
+
+
+// Every function in FP is based on Lambda calculus.
+// According to it, a function takes 1 input and must
+// 1 output and nothing else.
+// print() is not a pure function
+// viewDidLoad() too is not a pure function
+
+// 1. currying function to act as if it takes 2 parameters
+// This allows for partial application
+func add(_ a: Int) -> (Int) -> Int {
+    return { b in
+        return a + b
+    }
+}
+
+
+// Simple functions
+func mult5(_ a: Int) -> Int {
+    return a * 5
+}
+
+func mult(_ a: Int, _ b: Int) -> Int {
+    return a + b
+}
+
+
+
+// Functor
+// Anything that is mappable
+// A functor has to map from the same kind
+// Result<T> map should produce Result<U> but not [U]
+// although [U] is a functor (list functor)
+
+
+// map : (a -> b) -> Functor a -> Functor b
+//
+// func map(_ transform: (A -> B), on: Functor<A>) -> Functor<B>
+
+
+
+
+
+
 
